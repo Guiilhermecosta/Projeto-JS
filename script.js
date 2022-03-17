1e026505abdd4e9cb22688517d8effb4c0d3edad
Guilheme do C. Costa <gguillhherrmmecosta@gmail.com>
2022-03-17 18:49:57 -0300

    Update script.js
    Add peso aos horários..

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-msg.innerHTML = `Bom dia! agora são ${hora} horas.`
+msg.innerHTML = `Bom dia! agora são <strong>${hora}</strong> horas.`
 if (hora >= 0 && hora <12) {
     img.src = 'fotomanha.png'
     document.body.style.background = '#009fff'
 } else if (hora >= 12 && hora <18) {
     img.src = 'fototarde.png'
     document.body.style.background = '#b9846f'
-    msg.innerHTML = `Boa tarde! agora são ${hora} horas`
+    msg.innerHTML = `Boa tarde! agora são <strong>${hora}</strong> horas`
 } else {
     img.src = 'fotonoite.png'
     document.body.style.background = '#515154'
-    msg.innerHTML = `Boa noite! agora são ${hora} horas`
+    msg.innerHTML = `Boa noite! agora são <strong>${hora}</strong> horas`
 }
 }
